9a7fe730aec4f335b1023f5fdb15ca3b49f41b81
Nguyễn Đình Minh Đăng <minhdang22082003@gmail.com>
2026-01-01 11:33:39 +0700

    refactor: optimize loop syntax and map logic for Group Anagrams

--- 01. Arrays-Hashing/Medium-Group-Anagrams.js
+++ 01. Arrays-Hashing/Medium-Group-Anagrams.js
@@ -1,5 +1,4 @@
 
-// TODO: Refactor loop
 class Solution {
   /**
    * @param {string[]} strs
@@ -7,18 +6,17 @@ class Solution {
    */
   groupAnagrams(strs) {
     const group = new Map();
-    for (let i = 0; i < strs.length; i++) {
+    for (const str of strs) {
       const count = new Array(26).fill(0);
-      for (const char of strs[i]) {
+      for (const char of str) {
         const index = char.charCodeAt(0) - "a".charCodeAt(0);
         count[index]++;
       }
       const key = count.toString();
-      if (group.has(key)) {
-        group.get(key).push(strs[i]);
-      } else {
-        group.set(key, [strs[i]]);
+      if (!group.has(key)) {
+        group.set(key, []);
       }
+      group.get(key).push(str);
     }
     return Array.from(group.values());
   }
